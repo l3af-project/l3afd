dafe00b83c6199b156e0f539568f4c905b1124fe
Santhosh Fernandes <santhosh.fernandes@gmail.com>
2022-10-12 12:28:53 +0530

    Validating Client Certificate SANs  (#96)
    Add mTLS SANs support
    
    Signed-off-by: Santhosh Fernandes <santhosh.fernandes@gmail.com>
    Co-authored-by: Jason Niesz <Jason.Niesz@walmart.com>

--- apis/configwatch.go
+++ apis/configwatch.go
@@ -11,14 +11,17 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"path"
+	"regexp"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	httpSwagger "github.com/swaggo/http-swagger"
 
@@ -33,9 +36,11 @@ import (
 )
 
 type Server struct {
-	KFRTConfigs *kf.NFConfigs
-	HostName    string
-	l3afdServer *http.Server
+	KFRTConfigs   *kf.NFConfigs
+	HostName      string
+	l3afdServer   *http.Server
+	CaCertPool    *x509.CertPool
+	SANMatchRules []string
 }
 
 // @title L3AFD APIs
@@ -52,6 +57,7 @@ func StartConfigWatcher(ctx context.Context, hostname, daemonName string, conf *
 		l3afdServer: &http.Server{
 			Addr: conf.L3afConfigsRestAPIAddr,
 		},
+		SANMatchRules: conf.MTLSSANMatchRules,
 	}
 
 	term := make(chan os.Signal, 1)
@@ -83,14 +89,33 @@ func StartConfigWatcher(ctx context.Context, hostname, daemonName string, conf *
 			if err != nil {
 				log.Fatal().Err(err).Msgf("client CA %s file not found", conf.MTLSCACertFilename)
 			}
-			caCertPool := x509.NewCertPool()
-			caCertPool.AppendCertsFromPEM(caCert)
 
-			// Create the TLS Config with the CA pool and enable Client certificate validation
+			s.CaCertPool, _ = x509.SystemCertPool()
+			if s.CaCertPool == nil {
+				s.CaCertPool = x509.NewCertPool()
+			}
+			if ok := s.CaCertPool.AppendCertsFromPEM(caCert); !ok {
+				log.Warn().Msgf("No client certs appended for mTLS")
+			}
+			serverCertFile := path.Join(conf.MTLSCertDir, conf.MTLSServerCertFilename)
+			serverKeyFile := path.Join(conf.MTLSCertDir, conf.MTLSServerKeyFilename)
+			serverCert, err := tls.LoadX509KeyPair(serverCertFile, serverKeyFile)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("failure loading certs")
+			}
+			// build server config
 			s.l3afdServer.TLSConfig = &tls.Config{
-				ClientCAs:  caCertPool,
-				ClientAuth: tls.RequireAndVerifyClientCert,
-				MinVersion: conf.MTLSMinVersion,
+				Certificates: []tls.Certificate{serverCert},
+				GetConfigForClient: func(hi *tls.ClientHelloInfo) (*tls.Config, error) {
+					serverConf := &tls.Config{
+						Certificates:          []tls.Certificate{serverCert},
+						MinVersion:            tls.VersionTLS12,
+						ClientAuth:            tls.RequireAndVerifyClientCert,
+						ClientCAs:             s.CaCertPool,
+						VerifyPeerCertificate: s.getClientValidator(hi),
+					}
+					return serverConf, nil
+				},
 			}
 
 			cpb, _ := pem.Decode(caCert)
@@ -114,7 +139,7 @@ func StartConfigWatcher(ctx context.Context, hostname, daemonName string, conf *
 				}
 			}()
 
-			if err := s.l3afdServer.ListenAndServeTLS(path.Join(conf.MTLSCertDir, conf.MTLSServerCertFilename), path.Join(conf.MTLSCertDir, conf.MTLSServerKeyFilename)); err != nil {
+			if err := s.l3afdServer.ListenAndServeTLS(serverCertFile, serverKeyFile); err != nil {
 				log.Fatal().Err(err).Msgf("failed to start L3AFD server with mTLS enabled")
 			}
 		} else {
@@ -167,10 +192,10 @@ func MonitorTLS(start time.Time, expiry time.Time, conf *config.Config) {
 	expiryDate := expiry
 	startDate := start
 	diff := expiryDate.Sub(todayDate)
-	remaingHoursToStart := todayDate.Sub(startDate)
+	remainingHoursToStart := todayDate.Sub(startDate)
 	limit := conf.MTLSCertExpiryWarningDays * 24
 	remainingHoursToExpire := int(diff.Hours())
-	if remaingHoursToStart > 0 {
+	if remainingHoursToStart > 0 {
 		log.Fatal().Msgf("tls certificate start from : %v", startDate)
 	}
 	if remainingHoursToExpire <= limit {
@@ -180,4 +205,152 @@ func MonitorTLS(start time.Time, expiry time.Time, conf *config.Config) {
 			log.Warn().Msgf("tls certificate will expire in %v days", int64(remainingHoursToExpire/24))
 		}
 	}
+}
+
+func (s *Server) getClientValidator(helloInfo *tls.ClientHelloInfo) func([][]byte, [][]*x509.Certificate) error {
+
+	log.Debug().Msgf("Inside get client validator - %v", helloInfo.Conn.RemoteAddr())
+	return func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+		// Verifying client certs with root ca
+		opts := x509.VerifyOptions{
+			Roots:         s.CaCertPool,
+			CurrentTime:   time.Now(),
+			Intermediates: x509.NewCertPool(),
+			KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		}
+		_, err := verifiedChains[0][0].Verify(opts)
+		if err != nil {
+			log.Error().Err(err).Msgf("certs verification failed")
+			return err
+		}
+
+		log.Debug().Msgf("validating with SAN match rules - %s", s.SANMatchRules)
+		if len(s.SANMatchRules) == 0 {
+			return nil
+		}
+		for _, dnsName := range verifiedChains[0][0].DNSNames {
+			if !validHostname(dnsName, true) {
+				continue
+			}
+			dnsName = toLowerCaseASCII(dnsName)
+			for _, sanMatchRule := range s.SANMatchRules {
+				sanMatchRule = toLowerCaseASCII(sanMatchRule)
+				if matchExactly(dnsName, sanMatchRule) {
+					log.Debug().Msgf("Successfully matched matchExactly cert dns %s SANMatchRule %s", dnsName, sanMatchRule)
+					return nil
+				} else if matchHostnamesWithRegexp(dnsName, sanMatchRule) {
+					log.Debug().Msgf("Successfully matched matchHostnamesWithRegexp cert dns %s SANMatchRule %s", dnsName, sanMatchRule)
+					return nil
+				}
+			}
+		}
+
+		err = errors.New("certs verification with SAN match not found")
+		log.Error().Err(err).Msgf("SAN match rules %s", s.SANMatchRules)
+		return err
+	}
+}
+
+// toLowerCaseASCII returns a lower-case version of in. See RFC 6125 6.4.1. We use
+// an explicitly ASCII function to avoid any sharp corners resulting from
+// performing Unicode operations on DNS labels.
+func toLowerCaseASCII(in string) string {
+	// If the string is already lower-case then there's nothing to do.
+	isAlreadyLowerCase := true
+	for _, c := range in {
+		if c == utf8.RuneError {
+			// If we get a UTF-8 error then there might be
+			// upper-case ASCII bytes in the invalid sequence.
+			isAlreadyLowerCase = false
+			break
+		}
+		if 'A' <= c && c <= 'Z' {
+			isAlreadyLowerCase = false
+			break
+		}
+	}
+
+	if isAlreadyLowerCase {
+		return in
+	}
+
+	out := []byte(in)
+	for i, c := range out {
+		if 'A' <= c && c <= 'Z' {
+			out[i] += 'a' - 'A'
+		}
+	}
+	return string(out)
+}
+
+// validHostname reports whether host is a valid hostname that can be matched or
+// matched against according to RFC 6125 2.2, with some leniency to accommodate
+// legacy values.
+func validHostname(host string, isPattern bool) bool {
+	if !isPattern {
+		host = strings.TrimSuffix(host, ".")
+	}
+	if len(host) == 0 {
+		return false
+	}
+
+	for i, part := range strings.Split(host, ".") {
+		if part == "" {
+			// Empty label.
+			return false
+		}
+		if isPattern && i == 0 && part == "*" {
+			// Only allow full left-most wildcards, as those are the only ones
+			// we match, and matching literal '*' characters is probably never
+			// the expected behavior.
+			continue
+		}
+		for j, c := range part {
+			if 'a' <= c && c <= 'z' {
+				continue
+			}
+			if '0' <= c && c <= '9' {
+				continue
+			}
+			if 'A' <= c && c <= 'Z' {
+				continue
+			}
+			if c == '-' && j != 0 {
+				continue
+			}
+			if c == '_' {
+				// Not a valid character in hostnames, but commonly
+				// found in deployments outside the WebPKI.
+				continue
+			}
+			return false
+		}
+	}
+
+	return true
+}
+
+// matchExactly - match hostnames
+func matchExactly(hostA, hostB string) bool {
+	// Here checking hostB (i.e. sanMatchRule) is valid hostname and not regex/pattern
+	if !validHostname(hostB, false) {
+		return false
+	}
+	return hostA == hostB
+}
+
+// matchHostnamesWithRegexp - To match the san rules with regexp
+func matchHostnamesWithRegexp(dnsName, sanMatchRule string) bool {
+	defer func() bool {
+		if err := recover(); err != nil {
+			log.Warn().Msgf("panic occurred: %v", err)
+		}
+		return false
+	}()
+	if len(dnsName) == 0 || len(sanMatchRule) == 0 {
+		return false
+	}
+	re := regexp.MustCompile(sanMatchRule)
+
+	return re.MatchString(dnsName)
 }
